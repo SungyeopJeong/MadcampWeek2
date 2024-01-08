e2952aa47b354fa8d59a978e139b071d826343b4
SungyeopJeong <syeopj4a@gmail.com>
2024-01-08 11:20:50 +0900

    Add font system and logo

---
+++ lib/style/theme.dart
@@ -0,0 +1,9 @@
+import 'package:devil/style/color.dart';
+import 'package:flutter/material.dart';
+
+class DevilTheme {
+  static final theme = ThemeData(
+    fontFamily: 'Pretendard',
+    scaffoldBackgroundColor: DevilColor.white,
+  );
+}
